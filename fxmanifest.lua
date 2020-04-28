08d46532a4867138def5e8c1550b95963aaa36f0
Kalinka <34778006+KalinkaGit@users.noreply.github.com>
2020-04-28 03:41:49 +0200

    v1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+disable_lazy_natives 'yes'
+
+description 'vSyncRevamped'
+version '1.0.0'
+
+server_scripts {
+	'config.lua',
+	'locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'server/server.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'client/client.lua'
+}
