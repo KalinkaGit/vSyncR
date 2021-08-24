81e58f529f3cd4cfa352ed1d2937f14042b1fbab
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-08-24 14:56:59 +0100

    Globbed Locales

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,21 +7,13 @@ version '1.0.2'
 server_scripts {
 	'config.lua',
 	'locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/pt.lua', -- Thanks to raphapt https://github.com/raphapt
-	'locales/tr.lua', -- Thanks to thegambid https://github.com/thegambid
-	'locales/pt_br.lua', -- Thanks to Richards0nd https://github.com/Richards0nd
+	'locales/*.lua', -- Thanks to raphapt https://github.com/raphapt, thegambid https://github.com/thegambid and Richards0nd https://github.com/Richards0nd
 	'server/server.lua'
 }
 
 client_scripts {
 	'config.lua',
 	'locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/pt.lua', -- Thanks to raphapt https://github.com/raphapt
-	'locales/tr.lua', -- Thanks to thegambid https://github.com/thegambid
-	'locales/pt_br.lua', -- Thanks to Richards0nd https://github.com/Richards0nd
+	'locales/*.lua', -- Thanks to raphapt https://github.com/raphapt, thegambid https://github.com/thegambid and Richards0nd https://github.com/Richards0nd
 	'client/client.lua'
 }
